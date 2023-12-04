68797f69613571d2093e06d912f645ad1a2ec94b
K1ver <danil.puchkov.1993@mail.ru>
2023-12-04 13:31:02 +0700

    Added Hpbar and DeathScreen

--- Assets/Scripts/PlayerScrits/PlayerHealth.cs
+++ Assets/Scripts/PlayerScrits/PlayerHealth.cs
@@ -1,20 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Rendering;
+using TMPro;
+using System;
 
 public class PlayerHealth : MonoBehaviour
 {
     public float health { get; private set; }
+    private float MaxHp;
+    public GameObject DeathScreen;
+    public Image hpBarImage;
+    public TMP_Text textHpBar;
     // Start is called before the first frame update
     void Start()
     {
-        health = PlayerPrefs.GetFloat("MaxHP");
+        MaxHp = PlayerPrefs.GetFloat("MaxHP");
+        health = MaxHp;
     }
 
     public void StatsUpgrade()
     {
         health = PlayerPrefs.GetFloat("MaxHP") - (PlayerPrefs.GetFloat("MaxHP") - health);
+        MaxHp = PlayerPrefs.GetFloat("MaxHP");
+        UpdateHPBarValue();
     }
 
     // Update is called once per frame
@@ -23,9 +33,17 @@ public class PlayerHealth : MonoBehaviour
 
     }
 
+    public void UpdateHPBarValue()
+    {
+        float coef = health / MaxHp;
+        hpBarImage.fillAmount = coef;
+        textHpBar.text = Convert.ToString(health + " / " + MaxHp);
+    }
+
     public void TakeDamage(int damage)
     {
         health -= damage;
+        UpdateHPBarValue();
         if (health <= 0)
         {
             Die();
@@ -35,7 +53,11 @@ public class PlayerHealth : MonoBehaviour
 
     private void Die()
     {
-        //print("Die");
+        if (!DeathScreen.activeSelf)
+        {
+            DeathScreen.SetActive(true);
+            PlayerPrefs.SetFloat("HeroSpeed", 0);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
